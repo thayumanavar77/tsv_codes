a21d0527e415bf4ff05627a800d44cbd782ced41
Thayumanavar S <thayumk@gmail.com>
2016-06-12 23:52:24 +0530

    Paralle Blur Filter!

---
+++ scala/parallel/scalashop/src/main/scala/scalashop/package.scala
@@ -0,0 +1,73 @@
+
+import common._
+
+package object scalashop {
+
+  /** The value of every pixel is represented as a 32 bit integer. */
+  type RGBA = Int
+
+  /** Returns the red component. */
+  def red(c: RGBA): Int = (0xff000000 & c) >>> 24
+
+  /** Returns the green component. */
+  def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
+
+  /** Returns the blue component. */
+  def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
+
+  /** Returns the alpha component. */
+  def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
+
+  /** Used to create an RGBA value from separate components. */
+  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+    (r << 24) | (g << 16) | (b << 8) | (a << 0)
+  }
+
+  /** Restricts the integer into the specified range. */
+  def clamp(v: Int, min: Int, max: Int): Int = {
+    if (v < min) min
+    else if (v > max) max
+    else v
+  }
+
+  /** Image is a two-dimensional matrix of pixel values. */
+  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+    def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+    def apply(x: Int, y: Int): RGBA = data(y * width + x)
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
+  }
+
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    // TODO implement using while loops
+    if (radius == 0) return src.apply(x,y)
+    var redAvg = 0; var blueAvg = 0; var greenAvg = 0;  var alphaAvg = 0
+    var startx = clamp( x - radius, 0, src.width - 1)
+    var endx = clamp(x + radius, 0, src.width - 1)
+    var starty = clamp(y - radius, 0, src.height - 1)
+    var endy = clamp(y + radius, 0, src.height - 1)
+    var count=0
+
+    //println("BoxBlurKernel Method")
+
+
+    while (starty <= endy) {
+      startx=  clamp(x - radius, 0, src.width - 1)
+      while (startx <= endx) {
+        val pixelValue = src.apply(startx,starty)
+        redAvg+=red(pixelValue); blueAvg+=blue(pixelValue); greenAvg+=green(pixelValue)
+        alphaAvg+=alpha(pixelValue)
+        startx+=1
+        count+=1
+        //println("(x,y) + "+startx + "  " + starty)
+      }
+      starty+=1
+    }
+
+    //println(" End of while count"+count)
+    if (count == 0)
+    src.apply(x,y)
+    else rgba(redAvg/count.toInt, greenAvg/count,
+              blueAvg/count.toInt, alphaAvg/count)
+  }
+}
